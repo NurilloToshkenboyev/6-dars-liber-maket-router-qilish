ba868bd55d79797ea81cb133af0a64db0e9cdd0e
NurilloToshkenboyev <toshkenboyevnurillo819@gmail.com>
2024-09-14 09:41:23 +0500

    liber maket router qilish

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import  About  from "./about/page";
+export default function Home() {
+  return (<div>
+    
+    <section>
+    <About/>
+    </section>
+  </div>
+   
+  );
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='w-[100%] h-[200px] flex items-center justify-center text-white bg-black'>Footer</div>
+  )
+}
